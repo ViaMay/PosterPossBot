2a8f0d169842070896f508ce957f1fde8d95ba76
via <victoria.elizarian@skapis.co>
2019-09-24 11:08:07 +0500

    cron

--- cron-node.js
+++ cron-node.js
@@ -2,8 +2,11 @@ let cron = require('node-cron');
 const childProcess = require('child_process');
 const process = require('process');
 
-cron.schedule('00 00 0-23 * * *', () => {
+cron.schedule('00 00 10 * * *', () => {
     console.log("1");
    let log = childProcess.exec('npm test').toString();
    console.log(log);
+}, {
+    scheduled: true,
+    timezone: "Asia/Yekaterinburg"
 });
